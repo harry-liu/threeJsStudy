cc0ed9a596c3031bda9c5baa2b391d6012cee23a
代码新手 <747311978@qq.com>
2017-09-19 16:12:31 +0800

    1.0

--- js/index.js
+++ js/index.js
@@ -1,24 +1,60 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+let scene,camera,renderer,controls,light;
+let model;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 20000 );
+    camera.position.set(0,150,400);
+    scene.add(camera);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    renderer = new THREE.WebGLRenderer({
+        antialias:true,
+        preserveDrawingBuffer: true
+    });
+    renderer.setClearColor(0xffffff,1);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+    light  = new THREE.PointLight(0xffffff);
+    light.position.set(0,150,100);
+    scene.add(light);
 
-var animate = function () {
+    let sphereGeom = new THREE.SphereGeometry(50,32,16);
+
+    let darkMaterial = new THREE.MeshBasicMaterial({ color:0x000088 });
+    let darkMaterialL = new THREE.MeshLambertMaterial({ color:0x000088 });
+    let darkMaterialP = new THREE.MeshPhongMaterial({ color:0x000088 });
+
+    let modelL = new THREE.Mesh(sphereGeom.clone(),darkMaterial);
+    modelL.position.set(-150,50,0);
+    scene.add(modelL);
+
+    let modelM = new THREE.Mesh(sphereGeom.clone(),darkMaterialL);
+    modelM.position.set(0,50,0);
+    scene.add(modelM);
+
+    let modelR = new THREE.Mesh(sphereGeom.clone(),darkMaterialP);
+    modelR.position.set(150,50,0);
+    scene.add(modelR);
+
+    let lightbulb = new THREE.Mesh(
+        new THREE.SphereGeometry( 10, 16, 8 ),
+        new THREE.MeshBasicMaterial( { color: 0xffaa00 } )
+    );
+    lightbulb.position.set(0,150,100);
+    scene.add( lightbulb );
+
+    controls = new THREE.OrbitControls(camera,renderer.domElement);
+    controls.target.set(0,0,0);
+    controls.enableDamping = true;
+}
+
+function animate() {
     requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
     renderer.render(scene, camera);
-};
+    controls.update();
+}
+
+init();
 
 animate();
